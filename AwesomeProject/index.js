6c1ca6e7f77523764467bcab42bf6dda12024538
yongqing <490844594@qq.com>
2019-11-20 10:30:24 +0800

    Update

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -6,5 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './src/app.js';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler'
-
+ 
 AppRegistry.registerComponent(appName, () => App);
