36bb01e376e336bb2e928e90f458ba3acb247e06
yongqing <490844594@qq.com>
2019-11-22 11:29:35 +0800

    Update

--- AwesomeProject/src/screens/webview/index.js
+++ AwesomeProject/src/screens/webview/index.js
@@ -2,7 +2,7 @@
  * @Author: yongqing
  * @Date:   2019-11-19 11:00:02
  * @Last Modified by:   yongqing
- * @Last Modified time: 2019-11-19 11:41:57
+ * @Last Modified time: 2019-11-22 11:12:15
  */
 
 import React, { Component } from 'react';
@@ -15,10 +15,12 @@ class AppWebView extends Component {
             let { height, width } = Dimensions.get('window');
             return (
                     <WebView
-				        source={{ uri: 'https://m.baidu.com' }}
+				        source={{ uri: 'http://127.0.0.1:8080/index.html' }}
 				        javaScriptEnabled={true}
 				        startInLoadingState={true}
-				        scalesPageToFit={true}
+				        scalesPageToFit={false}
+				        applicationNameForUserAgent={'DemoApp/1.1.0'}
+
 				        renderError={()=>{
 				        	return (
 				        		<View style={{position:'absolute',height:200,width:width,textAlign:'center',textAlignVertical:'center',top:100}}> 
